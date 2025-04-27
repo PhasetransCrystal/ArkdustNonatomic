4bbee1ad51e23ff8eef2bd59c437b35f1648f919
AW-CRK14 <1731936512@qq.com>
2025-04-27 13:08:56 +0800

    资源提取标准化流程部分基本完成

--- src/main/java/com/phasetranscrystal/nonard/migrate/igmater_supplier/IGMaterialExtractor.java
+++ src/main/java/com/phasetranscrystal/nonard/migrate/igmater_supplier/IGMaterialExtractor.java
@@ -1,8 +1,13 @@
 package com.phasetranscrystal.nonard.migrate.igmater_supplier;
 
+import com.google.common.collect.ImmutableMap;
 import com.mojang.serialization.Codec;
 import com.phasetranscrystal.nonard.ArkdustNonatomic;
 import net.minecraft.resources.ResourceLocation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public interface IGMaterialExtractor<T> {
     ResourceLocation NAME = ArkdustNonatomic.location("igm_extractor");
@@ -13,13 +18,40 @@ public interface IGMaterialExtractor<T> {
 
     double requestCount();
 
-    /**从提供的资源供应器中提取所需要的内容
+    double extractForm(boolean simulate,double countLimit, IGMaterialSupplier<T> extractor);
+
+    /**
+     * 从提供的资源供应器中提取所需要的内容
+     *
      * @param simulate 是否为模拟，如果为true则只计数，不更改供应器内数据
      */
-    default double extractFrom(boolean simulate, IGMaterialSupplier<?>... suppliers){
-        //TODO 调度 见IGMSup.Alternate
-        return 0;
+    default double extractFrom(boolean simulate, IGMaterialSupplier<?>... suppliers) {
+        List<IGMaterialSupplier<T>> list = new ArrayList<>(suppliers.length);
+        ImmutableMap<Class, IGMaterialSupplier.Dispatcher> map = IGMaterialController.IGM_DISPATCHER_MAP.get().getOrDefault(targetClass(), ImmutableMap.of());
+        for (IGMaterialSupplier<?> sup : suppliers) {
+            if (targetClass().isAssignableFrom(sup.targetClass())) {
+                list.add((IGMaterialSupplier<T>) sup);
+            } else if (map.containsKey(sup.targetClass())) {
+                list.add(map.get(sup.targetClass()).transform(sup));
+            }
+        }
+        return extractedFromDispatched(simulate, list);
     }
 
-    double extractedFromDispatched(boolean simulate, IGMaterialSupplier<T>... suppliers);
+    default double extractedFromDispatched(boolean simulate, List<IGMaterialSupplier<T>> suppliers){
+        double remain = 0;
+        for (IGMaterialSupplier<T> supplier : suppliers) {
+            remain -= extractForm(simulate, remain, supplier);
+            if (remain <= 0) {
+                return requestCount();
+            }
+        }
+        return remain;
+    };
+
+    default double extractedFromDispatched(boolean simulate, IGMaterialSupplier<T>... suppliers) {
+        return extractedFromDispatched(simulate, Arrays.asList(suppliers));
+    }
+
+    ;
 }
