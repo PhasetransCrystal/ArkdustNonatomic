9f33b3460aae72841f3cdf684d3487d535532db9
AW-CRK14 <1731936512@qq.com>
2025-09-09 14:02:58 +0800

    part部分堵车了

---
+++ src/main/java/com/phasetranscrystal/nonard/migrate/nona_quench/registry/DataComponentRegistry.java
@@ -0,0 +1,12 @@
+package com.phasetranscrystal.nonard.migrate.nona_quench.registry;
+
+import com.phasetranscrystal.nonard.migrate.nona_quench.BreaQuench;
+import net.minecraft.core.component.DataComponentType;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class DataComponentRegistry {
+    public static final DeferredRegister<DataComponentType<?>> REGISTER = DeferredRegister.createDataComponents(BreaQuench.MODID);
+
+    public static final DeferredHolder<>
+}
